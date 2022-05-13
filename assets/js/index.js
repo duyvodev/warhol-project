0a9533b725d6f9f4052f532355bb2365da0ae359
duyvodev <duy.vo@nfq.asia>
2022-05-13 15:45:27 +0700

    MODIFIED: styles Header

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
 // let slider = document.querySelector(".slider")
 // let content = document.querySelector(".content")
 // let footer = document.querySelector(".footer-section")
+let header = document.querySelector(".header-wrapper")
 //
 // window.onwheel = e => {
 //     if(e.deltaY >= 0){
@@ -13,3 +14,13 @@
 //     }
 // }
 
+
+window.onwheel = e => {
+    if(e.deltaY >= 0){
+        // Scrolling Down with mouse
+        header.classList.add("header--hide")
+    } else {
+        // Scrolling Up with mouse
+        header.classList.remove("header--hide")
+    }
+}
